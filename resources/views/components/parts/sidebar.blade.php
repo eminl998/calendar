3c84a02c28a67a992cfbef5e0fa4a740e989ce6b
Emin Lika <eminlika998@gmail.com>
2023-04-03 16:13:43 +0200

    03.04.2023

--- resources/views/components/parts/sidebar.blade.php
+++ resources/views/components/parts/sidebar.blade.php
@@ -19,6 +19,17 @@
             @include('components.parts.popUp')
          </li>
          
+         <li> 
+         <a class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" onclick="openModal()" >
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" viewBox="0 0 20 20" fill="currentColor">
+               <path d="M18,3H17V1a1,1,0,0,0-2,0V3H5V1A1,1,0,0,0,3,1V3H2A2,2,0,0,0,0,5V16a2,2,0,0,0,2,2H18a2,2,0,0,0,2-2V5A2,2,0,0,0,18,3ZM4,6A1,1,0,0,1,5,5H6A1,1,0,0,1,6,7H5A1,1,0,0,1,4,6Zm4,0A1,1,0,0,1,9,5h1A1,1,0,0,1,10,7H9A1,1,0,0,1,8,6Zm4,0A1,1,0,0,1,13,5h1a1,1,0,0,1,0,2H13A1,1,0,0,1,12,6Z" />
+
+            </svg>
+            <span class="ml-3">Holiday Vacations</span>
+         </a>
+         @include('components.parts.vacations')
+         </li>
+         
          <li>
             <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
