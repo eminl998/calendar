47a6d57d71bdeedb88ad09d7053877495e57cede
Emin Lika <eminlika998@gmail.com>
2023-04-07 08:53:32 +0200

    07.04.2023

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
             @endif
                 {{-- Kalendari --}}
                 <div class="max-w-7xl mx-auto p-6 lg:p-8 bg-gray-100 dark:bg-gray-800 text-black dark:text-red-500 rounded-md">
-                    @include('components.parts.calendar')
+                    @include('components.parts.calendar1')
                 </div>
         </div>
     </body>
