f542abe39dcd527898e3e2800c3198fe93a8d58b
Emin Lika <eminlika998@gmail.com>
2023-04-07 14:45:59 +0200

    07.04.2023

--- resources/views/holidays.blade.php
+++ resources/views/holidays.blade.php
@@ -70,7 +70,11 @@
                                         </td>
                                          @if (Auth::user()->is_admin == 1)
                                          <td class="px-10 py-6">
-                                            Edit or Delete button
+                                            <form method="POST" action="{{ route('holidays.destroy', $holiday->id) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="border-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm hover:bg-red-500 hover:text-white font-bold py-2 px-4">Delete</button>
+                                            </form>
                                          </td>
                                          @endif
                                     </tr>
