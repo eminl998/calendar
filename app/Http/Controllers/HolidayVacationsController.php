0d01d52c130bacf1292e793dbe1f7e22f3403311
Emin Lika <eminlika998@gmail.com>
2023-04-07 15:46:28 +0200

    07.04.2023

--- app/Http/Controllers/HolidayVacationsController.php
+++ app/Http/Controllers/HolidayVacationsController.php
@@ -27,24 +27,24 @@ class HolidayVacationsController extends Controller
     }
 
 
-    public function update(Request $request, $id)
+    // public function update(Request $request, $id)
+    // {
+    //     $holiday = HolidayVacation::find($id);
+    //     $holiday->title = $request->input('title');
+    //     $holiday->holiday_date = $request->input('holiday_date');
+    //     $holiday->rest_date = $request->input('rest_date');
+    //     $holiday->save();
+
+    //     return redirect()->route('holidays.index')->with('success', 'Holiday updated successfully');
+    // }
+
+
+    public function destroy($title)
     {
-        $holiday = HolidayVacation::find($id);
-        $holiday->title = $request->input('title');
-        $holiday->holiday_date = $request->input('holiday_date');
-        $holiday->rest_date = $request->input('rest_date');
-        $holiday->save();
-
-        return redirect()->route('holidays.index')->with('success', 'Holiday updated successfully');
-    }
-
-
-    public function destroy($id)
-    {
-        $holiday = HolidayVacation::find($id);
+        $holiday = HolidayVacation::find($title);
         $holiday->delete();
 
-        return response()->json(['success' => true]);
+        return redirect()->route('holidays.index')->with('success', 'Holiday deleted successfully');;
     }
 
 }
