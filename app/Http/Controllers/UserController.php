8627eedee60f2a7ee99987adc9eba748d04b9738
Emin Lika <eminlika998@gmail.com>
2023-04-19 12:43:33 +0200

    19.04.2023

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,8 +39,8 @@ class UserController extends Controller
         $holidays = HolidayVacation::pluck('rest_date')->toArray();
         $daysOff = 0;
         foreach ($approvedRequests as $request) {
-            $start_date = Carbon::parse($request->start_date);
-            $end_date = Carbon::parse($request->end_date);
+            $start_date = Carbon::parse($request->start_date)->startOfDay();
+            $end_date = Carbon::parse($request->end_date)->endOfDay();
             for ($date = $start_date; $date <= $end_date; $date->addDay()) {
                 if (!$date->isWeekend() && !in_array($date->format('Y-m-d'), $holidays)) {
                     $daysOff++;
